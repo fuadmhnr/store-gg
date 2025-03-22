13285c0ec32544b445846b85ea88c9baf9e3f3fb
fuadmhnr <fuadmhnr@gmail.com>
2025-03-22 09:16:05 +0700

    [feat] integrating sign up photo page

---
+++ app/(auth)/sign-up/photo/layout.tsx
@@ -0,0 +1,10 @@
+import { ReactNode } from "react";
+import "../../../../styles/pages/sign-up-photo.css";
+
+export default function SignUpPhotoLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  return <>{children}</>;
+}
