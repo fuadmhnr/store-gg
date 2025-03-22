858c50d64ab62b56ff63be4b6d3c7703c847bf61
fuadmhnr <fuadmhnr@gmail.com>
2025-03-22 13:52:50 +0700

    [feat] integration dashboard member overview

---
+++ app/(member)/member/layout.tsx
@@ -0,0 +1,10 @@
+import { ReactNode } from "react";
+import "@/styles/pages/sidebar.css";
+
+export default function MemberOverviewLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  return <>{children}</>;
+}
